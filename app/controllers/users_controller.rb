4081356ebe3195fc6d44fafc534e8a73f796bc83
varsha raikwar <varsharaikwar994@gmail.com>
2024-02-05 18:48:25 +0530

    minor enhencements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def create
       user = User.new(user_params)
         if user.save
-          render json: { user_id: user.id }, status: :created
+          render json: { user_id: user.id, message: "User Registration Successful" }, status: :created
         else
           render json: { error: user.errors.full_messages }, status: :unprocessable_entity
         end
